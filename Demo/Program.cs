ad1c2d9a077cc9e145eaf114356543fa4c9ee2fe
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-16 21:23:59 +0200

    String And StringBuilder

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,72 @@
-﻿namespace Demo
+﻿using System.Text;
+
+namespace Demo
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
+            #region String
+
+            //string S01;
+
+            ////S01 = new string("Ahmed");
+            //S01 = "Ahmed";
+            //Console.WriteLine(S01.GetHashCode());
+
+            //S01 = "ali";
+            //Console.WriteLine(S01.GetHashCode());
+
+            //S01 = "yassmine";
+            //Console.WriteLine(S01.GetHashCode());
+
+            //string Message = "Hello";
+            //Console.WriteLine("Message: "+Message); 
+            //Console.WriteLine("HashCode= "+Message.GetHashCode());
+
+            //Console.WriteLine();
+
+            //Message += " ahmed";
+            //Console.WriteLine("Message: " + Message);
+            //Console.WriteLine("HashCode= " + Message.GetHashCode());
+
+            #endregion
+
+            #region StringBuilder
+            //StringBuilder Message = new StringBuilder("Hello ");
+            //Console.WriteLine("Message: " + Message);
+            //Console.WriteLine("HashCode= "+Message.GetHashCode());
+
+            //Console.WriteLine();
+
+            //Message.Append(" ahmed");
+            //Console.WriteLine("Message: " + Message);
+            //Console.WriteLine("HashCode= " + Message.GetHashCode());
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
